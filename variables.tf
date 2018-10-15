6585f68dad55f84a2bcbbd0608a7e68276629f2e
Richard Worwood - Valtech DevOps <richard.worwood@engineering.digital.dwp.gov.uk>
2018-10-15 12:24:41 +0100

    Updated to expose kms_master_key_id parameter to allow user defined master keys

--- variables.tf
+++ variables.tf
@@ -47,3 +47,8 @@ variable "force_destroy" {
   description = "A boolean that indicates the bucket can be destroyed even if it contains objects. These objects are not recoverable"
   default     = "false"
 }
+
+variable "kms_master_key_id" {
+  description = "The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms"
+  default     = ""
+}
